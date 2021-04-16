03080f71b4e3491c9095384a960c106876528863
Natnael Tilaye <natnaeltilaye30@gmail.com>
2021-04-16 19:41:19 +0300

    Updated title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,7 +43,7 @@
     <div class="container">
       <div class="header-container d-flex align-items-center">
         <div class="logo mr-auto w-25">
-          <h1 class="text-light"><a href="/"><span>{{ __("Society of Friends for Ethiopian Visual Arts") }}</span></a></h1>
+          <h1 class="text-light"><a href="/"><span>Society of Friends for Ethiopian Visual Arts</span> <span>የኢትዮጵያ የዕይታዊ ጥበባት ወዳጆች ማህበር</span></a></h1>
           <!-- Uncomment below if you prefer to use an image logo -->
           <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
         </div>

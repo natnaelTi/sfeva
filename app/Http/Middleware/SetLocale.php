dcf8cfd6c21cd9d5310c6eaef7cc90734d1bd5fb
Abel Mulugeta <abelmulugeta2014@gmail.com>
2021-04-16 20:28:24 +0300

    add locale middleware

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Http\Request;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $locale = $request->session()->get('locale', 'en');
+
+        App::setLocale($locale);
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,18 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // Route::post('/locale', 'Controller@changelocale')->name('locale');
 
 Route::post('/locale', function (Request $request) {
     $locale = $request->input('locale');
 
-        if (in_array($locale, ['en', 'am'])) {
-            $request->session()->put('locale', $locale);
-        }
+    if (in_array($locale, ['en', 'am'])) {
+        $request->session()->put('locale', $locale);
+    }
 
-        return redirect()->back();
+    return redirect()->back();
 })->name('locale');
 
 Route::get('/', function () {
